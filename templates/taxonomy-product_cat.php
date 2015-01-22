b29526a0c95b784ecb948ea52d49fd73044813d6
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-22 19:05:37 +0200

    taxonomy-product_cat & context

--- templates/taxonomy-product_cat.php
+++ templates/taxonomy-product_cat.php
@@ -6,11 +6,47 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-wc_get_template( 'archive-product.php' );
+Maera_Template::dependencies();
+
+global $product, $woocommerce_loop;
+
+// Store loop count we're currently on
+if ( empty( $woocommerce_loop['loop'] ) ) {
+	$woocommerce_loop['loop'] = 0;
+}
+
+// Store column count for displaying the grid
+if ( empty( $woocommerce_loop['columns'] ) ) {
+	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+}
+
+// Ensure visibility
+if ( ! $product || ! $product->is_visible() ) {
+	return;
+}
+
+// Increase loop count
+$woocommerce_loop['loop']++;
+
+// Extra post classes
+$classes = array();
+if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
+	$classes[] = 'first';
+}
+if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
+	$classes[] = 'last';
+}
+
+$context = Maera_Template::context();
+$context['classes'] = $classes;
+
+Maera_Template::header();
+Maera_Template::main('archive-product.twig', $context );
+Maera_Template::footer();
--- templates/taxonomy-product_tag.php
+++ templates/taxonomy-product_tag.php
@@ -15,6 +15,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 Maera_Template::dependencies();
 
+global $product, $woocommerce_loop;
+
+// Store loop count we're currently on
+if ( empty( $woocommerce_loop['loop'] ) ) {
+	$woocommerce_loop['loop'] = 0;
+}
+
+// Store column count for displaying the grid
+if ( empty( $woocommerce_loop['columns'] ) ) {
+	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+}
+
+// Ensure visibility
+// TODO
+// if ( ! $product || ! $product->is_visible() ) {
+// 	return;
+// }
+
+// Increase loop count
+$woocommerce_loop['loop']++;
+
+// Extra post classes
+$classes = array();
+if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
+	$classes[] = 'first';
+}
+if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
+	$classes[] = 'last';
+}
+
+$context = Maera_Template::context();
+$context['classes'] = $classes;
+
 Maera_Template::header();
-Maera_Template::main('archive-product.twig');
+Maera_Template::main('archive-product.twig', $context );
 Maera_Template::footer();
